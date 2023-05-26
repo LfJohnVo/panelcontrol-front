ab1ab61047f0c105647af32a93fedb4040f07090
Mauricio David Blancas Garcia <mauricio.blancas@becarios.silent4business.com>
2023-05-25 20:59:49 -0600

    Inicializando el proyecto panel de control

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+// module.exports = {
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:react/jsx-runtime",
+//     "plugin:react-hooks/recommended",
+//   ],
+//   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+//   settings: { react: { version: "18.2" } },
+//   plugins: ["react-refresh"],
+//   rules: {
+//     "react-refresh/only-export-components": "warn",
+//   },
+// };
+
+module.exports = {
+  env: {
+    commonjs: true,
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {},
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "react-hooks"],
+  ignorePatterns: ["node_modules/"],
+  rules: {},
+  settings: {
+    react: {
+      version: "latest", // "detect" automatically picks the version you have installed.
+    },
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <p>React app vite</p>
+    </>
+  );
+}
+
+export default App;
