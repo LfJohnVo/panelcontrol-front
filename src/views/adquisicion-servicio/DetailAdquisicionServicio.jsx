e484bf33489d7076426c53a094529af817bd0971
Mauricio  Blancas <mauricio.blancas@becarios.silent4business.com>
2023-06-06 00:10:12 -0600

    Agregando la vista de detalle de la adquisicion del servicio

---
+++ src/views/adquisicion-servicio/DetailAdquisicionServicio.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Grid, Container } from "@mui/material";
+import AdquisicionServicioDetail from "../../components/adquisicion-servicio/AdquisicionServicioDetail";
+import { titleModulAdquisicionServicio } from "../../common/text/TextTitle";
+
+function DetailAdquisicionServicio() {
+  const props = {
+    navigateLink: "/adquisiciones",
+    title: titleModulAdquisicionServicio.titleAdquisicionServicioDetail,
+    text: titleModulAdquisicionServicio.descriptionAdquisicionServicioDetail,
+  };
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Grid container spacing={3} rowSpacing={0}>
+          <Grid item xs={12} md={12} lg={12}>
+            <AdquisicionServicioDetail {...props} />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
+
+export default DetailAdquisicionServicio;
