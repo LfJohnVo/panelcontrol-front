0369000dd827b0ca1ea87a0f8ec0bd006c307ecd
Mauricio  Blancas <mauricio.blancas@becarios.silent4business.com>
2023-06-06 00:00:45 -0600

    Agregando la vista para editar la un cliente con un servicio

---
+++ src/views/adquisicion-servicio/EditAdquisicionServicio.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Grid } from "@mui/material";
+import FormEditAdquisicionServicio from "../../components/adquisicion-servicio/FormEditAdquisicionServicio";
+import { titleModulAdquisicionServicio } from "../../common/text/TextTitle";
+function EditAdquisicionServicio() {
+  const props = {
+    navigateLink: "/adquisiciones",
+    title: titleModulAdquisicionServicio.titleAdquisicionServicioEdit,
+    text: titleModulAdquisicionServicio.descriptionAdquisicionServicioEdit,
+  };
+
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Grid container spacing={3} rowSpacing={0}>
+          <Grid item xs={12} md={12} lg={12}>
+            <FormEditAdquisicionServicio {...props} />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
+
+export default EditAdquisicionServicio;
