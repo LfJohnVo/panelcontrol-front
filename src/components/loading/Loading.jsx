1f832f722ad162b921c6b60141fb27fc52f43bb9
Mauricio  Blancas <mauricio.blancas@becarios.silent4business.com>
2023-06-16 13:18:36 -0600

    Agregando el consumo de la api para la tabla de usuarios donde se mostraran los usuarios

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
+import { Box, Paper, Grid } from "@mui/material/";
 import LinearProgress, {
   linearProgressClasses,
 } from "@mui/material/LinearProgress";
@@ -20,8 +20,35 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
 
 export default function Loading() {
   return (
-    <Box sx={{ flexGrow: 16 }}>
-      <BorderLinearProgress variant="indeterminate" />
-    </Box>
+    <Grid
+      item
+      xs={12}
+      md={12}
+      lg={12}
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={1}
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          height: 60,
+          width: 600,
+          background: "#FFFFF",
+        }}
+      >
+        <Box sx={{ flexGrow: 16 }}>
+          <BorderLinearProgress variant="indeterminate" />
+        </Box>
+      </Paper>
+    </Grid>
   );
 }
