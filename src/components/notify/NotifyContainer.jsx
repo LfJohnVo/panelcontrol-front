857f1bc2645101478c9fd8fafe28e903f4d70d80
Mauricio  Blancas <mauricio.blancas@becarios.silent4business.com>
2023-06-16 13:23:10 -0600

    Agregando el consumo de api para el formulario de usuarios

---
+++ src/components/notify/NotifyContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+function NotifyContainer() {
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+      />
+    </>
+  );
+}
+
+export default NotifyContainer;
